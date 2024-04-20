89987549410779696b6db6c4f4195f1b7ad6e5e4
Gabriel Salas <ggsalas@gmail.com>
2024-04-20 13:48:52 -0300

    Test nextAuth

--- app/auth.config.ts
+++ app/auth.config.ts
@@ -17,7 +17,7 @@ export const authConfig = {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/app', nextUrl));
+        return Response.redirect('/app');
       }
 
       return true;
